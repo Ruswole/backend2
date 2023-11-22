9c42e394e2a14250726601c7eb0963235b98faf6
Ruswole <norlydude@gmail.com>
2023-11-22 21:33:20 +0800

    Add Index and Destroy API for Users

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CarouselItemsController;
-
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,5 +25,9 @@ Route::get('/carousel/{id}',[CarouselItemsController::class, 'show']);
 Route::post('/carousel',[CarouselItemsController::class, 'store']);
 Route::put('/carousel/{id}',[CarouselItemsController::class, 'update']);
 Route::delete('/carousel/{id}',[CarouselItemsController::class, 'destroy']);
+
+Route::get('/user',[UserController::class, 'index']);
+Route::get('/user/{id}',[UserController::class, 'show']);
+Route::delete('/user/{id}',[UserController::class, 'destroy']);
 
 
